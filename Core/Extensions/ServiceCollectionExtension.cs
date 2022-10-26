096317a3badecea7f16a74dfa02d680cb2416580
Mustafa YILMAZ <75275308+yilmazmustafayilmaz@users.noreply.github.com>
2022-10-26 19:37:33 +0300

    Add files via upload

---
+++ Core/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection services, ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(services);
+            }
+            return ServiceTool.Create(services);
+        }
+    }
+}
---
+++ Core/Utilities/Helpers/FileHelpers/FileHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Core.Utilities.Helpers.FileHelpers
+{
+    public class FileHepler : IFileHelper
+    {
+        /// <summary>
+        /// File Delete
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+        }
+
+        /// <summary>
+        ///  File Update
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="filePath"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public string Update(IFormFile file, string filePath, string root)
+        {
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            return Upload(file, root);
+        }
+
+        /// <summary>
+        /// File Upload
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public string Upload(IFormFile file, string root)
+        {
+            if (file.Length > 0)
+            {
+                if (!Directory.Exists(root))
+                    Directory.CreateDirectory(root);
+
+                string extension = Path.GetExtension(file.FileName);
+                string guid = Guid.NewGuid().ToString();
+                string filePath = guid + extension;
+
+                using (FileStream fileStream = File.Create(root + filePath))
+                {
+                    file.CopyTo(fileStream);
+                    fileStream.Flush();
+                    return filePath;
+                }
+            }
+            return null;
+        }
+    }
+}
