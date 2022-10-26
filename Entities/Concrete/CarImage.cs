493cd4b24cef58534f0fb387b685a01d77c8a926
Mustafa YILMAZ <75275308+yilmazmustafayilmaz@users.noreply.github.com>
2022-10-26 19:32:24 +0300

    Add files via upload

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Concrete
+{
+    public class CarImage : IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public string ImagePath { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public IFormFile File { get; set; }
+    }
+}
